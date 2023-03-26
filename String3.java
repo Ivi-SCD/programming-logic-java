51ea9775a4f32193ea8fbff9d216a63d9c6333c4
bhavesh yadav <128934953+bhaVesh080@users.noreply.github.com>
2023-03-26 23:08:52 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,11 @@
+package facil;
+
+public class String3 {
+  public static void main(String[] args) {
+    String[] myArray = new String[3];
+    myArray[0] = "Hey";
+    myArray[1] = "hi";
+    myArray[2] = "there";
+    System.out.println(myArray[1]); // Output: World
+  }
+}
