dc70b66046b55e9663c358a104d3c6f38d207d1c
Ivisson Pereira <ivipnascimento@hotmail.com>
2023-05-11 22:14:37 +0000

    Resolucao da prova

---
+++ Listas de Exercicios/ProvaAV1/resolucaoComFor.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class resolucaoComFor {
+    public static void main(String[] args) {
+        final int tamanho = 5;
+
+        // Declaração dos arrays
+        String[] nomes = new String[tamanho];
+        float[] notaUm = new float[tamanho];
+        float[] notaDois = new float[tamanho];
+        float[] notaTres = new float[tamanho];
+
+        Scanner sc = new Scanner(System.in);
+
+        // Armazenamento dos valores
+        for (int i = 0; i < tamanho; i++) {
+            
+            if(i!= 0) {
+                sc.nextLine();
+            }
+
+            System.out.print("Digite o nome do aluno (" + (i+1) + "): ");
+            nomes[i] = sc.nextLine();
+
+            System.out.print("Digite a nota da unidade 1 para o aluno " + nomes[i] + ": ");
+            notaUm[i] = sc.nextFloat();
+
+            System.out.print("Digite a nota da unidade 2 para o aluno " + nomes[i] + ": ");
+            notaDois[i] = sc.nextFloat();
+
+            System.out.print("Digite a nota da unidade 3 para o aluno " + nomes[i] + ": ");
+            notaTres[i] = sc.nextFloat();
+        }
+
+        // Ordenação das notas de cada aluno em ordem crescente
+        for (int i = 0; i < tamanho; i++) {
+
+            
+            if (notaUm[i] > notaDois[i]) {
+                float temp = notaUm[i];
+                notaUm[i] = notaDois[i];
+                notaDois[i] = temp;
+            }
+            if (notaDois[i] > notaTres[i]) {
+                float temp = notaDois[i];
+                notaDois[i] = notaTres[i];
+                notaTres[i] = temp;
+            }
+            if (notaUm[i] > notaDois[i]) {
+                float temp = notaUm[i];
+                notaUm[i] = notaDois[i];
+                notaDois[i] = temp;
+            }
+        }
+
+        // Remoção da nota do último aluno
+        notaTres[tamanho - 1] = 0.0f;
+
+        //Exibição das notas
+        for(int i = 0; i < tamanho; i++) {
+            System.out.println("Nome do Aluno: " + nomes[i]);
+            System.out.println("Nota Um: " + notaUm[i]);
+            System.out.println("Nota Dois: " + notaDois[i]);
+            System.out.println("Nota Tres: " + notaTres[i]);
+        }
+    }
+}
---
+++ Listas de Exercicios/ProvaAV1/resolucaoSemFor.java
@@ -0,0 +1,174 @@
+import java.util.*;
+
+public class resolucaoSemFor {
+    public static void main(String[] args) {
+        final int tamanho = 5;
+
+        // Declaração dos arrays
+        String[] nomes = new String[tamanho];
+        float[] notaUm = new float[tamanho];
+        float[] notaDois = new float[tamanho];
+        float[] notaTres = new float[tamanho];
+
+        // Armazenamento dos valores
+        System.out.print("Digite o nome do aluno 1: ");
+        Scanner sc = new Scanner(System.in);
+        nomes[0] = sc.nextLine();
+
+        System.out.print("Digite a nota da unidade 1 para o aluno " + nomes[0] + ": ");
+        notaUm[0] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 2 para o aluno " + nomes[0] + ": ");
+        notaDois[0] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 3 para o aluno " + nomes[0] + ": ");
+        notaTres[0] = sc.nextFloat();
+
+        System.out.print("Digite o nome do aluno 2: ");
+        sc.nextLine();
+        nomes[1] = sc.nextLine();
+
+        System.out.print("Digite a nota da unidade 1 para o aluno " + nomes[1] + ": ");
+        notaUm[1] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 2 para o aluno " + nomes[1] + ": ");
+        notaDois[1] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 3 para o aluno " + nomes[1] + ": ");
+        notaTres[1] = sc.nextFloat();
+
+        System.out.print("Digite o nome do aluno 3: ");
+        sc.nextLine();
+        nomes[2] = sc.nextLine();
+
+        System.out.print("Digite a nota da unidade 1 para o aluno " + nomes[2] + ": ");
+        notaUm[2] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 2 para o aluno " + nomes[2] + ": ");
+        notaDois[2] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 3 para o aluno " + nomes[2] + ": ");
+        notaTres[2] = sc.nextFloat();
+
+        System.out.print("Digite o nome do aluno 4: ");
+        sc.nextLine();
+        nomes[3] = sc.nextLine();
+
+        System.out.print("Digite a nota da unidade 1 para o aluno " + nomes[3] + ": ");
+        notaUm[3] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 2 para o aluno " + nomes[3] + ": ");
+        notaDois[3] = sc.nextFloat();
+
+        System.out.print("Digite a nota da unidade 3 para o aluno " + nomes[3] + ": ");
+        notaTres[3] = sc.nextFloat();
+
+        if (notaUm[0] > notaDois[0]) {
+            float temp = notaUm[0];
+            notaUm[0] = notaDois[0];
+            notaDois[0] = temp;
+        }
+        if (notaUm[0] > notaTres[0]) {
+            float temp = notaUm[0];
+            notaUm[0] = notaTres[0];
+            notaTres[0] = temp;
+        }
+        if (notaDois[0] > notaTres[0]) {
+            float temp = notaDois[0];
+            notaDois[0] = notaTres[0];
+            notaTres[0] = temp;
+        }
+
+        if (notaUm[1] > notaDois[1]) {
+            float temp = notaUm[1];
+            notaUm[1] = notaDois[1];
+            notaDois[1] = temp;
+        }
+        if (notaUm[1] > notaTres[1]) {
+            float temp = notaUm[1];
+            notaUm[1] = notaTres[1];
+            notaTres[1] = temp;
+        }
+        if (notaDois[1] > notaTres[1]) {
+            float temp = notaDois[1];
+            notaDois[1] = notaTres[1];
+            notaTres[1] = temp;
+        }
+
+        if (notaUm[2] > notaDois[2]) {
+            float temp = notaUm[2];
+            notaUm[2] = notaDois[2];
+            notaDois[2] = temp;
+        }
+        if (notaUm[2] > notaTres[2]) {
+            float temp = notaUm[2];
+            notaUm[2] = notaTres[2];
+            notaTres[2] = temp;
+        }
+        if (notaDois[2] > notaTres[2]) {
+            float temp = notaDois[2];
+            notaDois[2] = notaTres[2];
+            notaTres[2] = temp;
+        }
+
+        if (notaUm[3] > notaDois[3]) {
+            float temp = notaUm[3];
+            notaUm[3] = notaDois[3];
+            notaDois[3] = temp;
+        }
+        if (notaUm[3] > notaTres[3]) {
+            float temp = notaUm[3];
+            notaUm[3] = notaTres[3];
+            notaTres[3] = temp;
+        }
+        if (notaDois[3] > notaTres[3]) {
+            float temp = notaDois[3];
+            notaDois[3] = notaTres[3];
+            notaTres[3] = temp;
+        }
+
+        if (notaUm[4] > notaDois[4]) {
+            float temp = notaUm[4];
+            notaUm[4] = notaDois[4];
+            notaDois[4] = temp;
+        }
+        if (notaUm[4] > notaTres[4]) {
+            float temp = notaUm[4];
+            notaUm[4] = notaTres[4];
+            notaTres[4] = temp;
+        }
+        if (notaDois[4] > notaTres[4]) {
+            float temp = notaDois[4];
+            notaDois[4] = notaTres[4];
+            notaTres[4] = temp;
+        }
+        
+        // Apagando a ultima nota inserido do quinto aluno.
+        notaTres[4] = 0;
+
+        System.out.println("Nome do Aluno: " + nomes[0]);
+        System.out.println("Nota Um: " + notaUm[0]);
+        System.out.println("Nota Dois: " + notaDois[0]);
+        System.out.println("Nota Tres: " + notaTres[0]);
+
+        System.out.println("Nome do Aluno: " + nomes[1]);
+        System.out.println("Nota Um: " + notaUm[1]);
+        System.out.println("Nota Dois: " + notaDois[1]);
+        System.out.println("Nota Tres: " + notaTres[1]);
+
+        System.out.println("Nome do Aluno: " + nomes[2]);
+        System.out.println("Nota Um: " + notaUm[2]);
+        System.out.println("Nota Dois: " + notaDois[2]);
+        System.out.println("Nota Tres: " + notaTres[2]);
+
+        System.out.println("Nome do Aluno: " + nomes[3]);
+        System.out.println("Nota Um: " + notaUm[3]);
+        System.out.println("Nota Dois: " + notaDois[3]);
+        System.out.println("Nota Tres: " + notaTres[3]);
+
+        System.out.println("Nome do Aluno: " + nomes[4]);
+        System.out.println("Nota Um: " + notaUm[4]);
+        System.out.println("Nota Dois: " + notaDois[4]);
+        System.out.println("Nota Tres: " + notaTres[4]);
+    }
+}
